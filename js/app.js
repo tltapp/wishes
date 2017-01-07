3c9e6547896123da9929670868ebfd87fcfe5cc8
vivinantony <vivinantony@gmail.com>
2017-01-07 18:18:19 +0530

    for testing

--- js/app.js
+++ js/app.js
@@ -137,7 +137,7 @@ app.controller('LoveCtrl', ['$scope', '$stateParams', '$firebaseArray', function
     }
 }]);
 
-app.controller('NewYearCtrl', ['$scope', '$stateParams', '$firebaseArray', function($scope, $stateParams, $firebaseArray) {
+app.controller('NewYearCtrl', ['$scope', '$stateParams', '$firebaseArray', '$location', function($scope, $stateParams, $firebaseArray, $location) {
     // var audio = new Audio('https://tltapp.github.io/wishes/tune/newyear.mp3');
     // audio.play();
 
@@ -154,6 +154,9 @@ app.controller('NewYearCtrl', ['$scope', '$stateParams', '$firebaseArray', funct
     $scope.share = {
         whatsapp: 'whatsapp://send?text=%2ANew Year Wishes%2A from %2A' + $stateParams.name + '%2A: https://tltapp.github.io/wishes/%23%21/newyear/' + $stateParams.name
     };
+
+    console.log('//////////' + $location.path());
+    console.log('##############' + $location.href());
 
     $scope.shareOnWhatsapp = function() {
         fb.$add({
@@ -192,64 +195,64 @@ app.controller('PongalCtrl', ['$scope', '$stateParams', '$firebaseArray', functi
 }]);
 
 function init1() {
-  var w = window.innerWidth,
-    h = window.innerHeight,
-    canvas = document.getElementById('snow'),
-    ctx = canvas.getContext('2d'),
-    rate = 50,
-    arc = 500,
-    time,
-    count,
-    size = 2,
-    speed = 10,
-    lights = new Array,
-    colors = ['#eee'];
+    var w = window.innerWidth,
+        h = window.innerHeight,
+        canvas = document.getElementById('snow'),
+        ctx = canvas.getContext('2d'),
+        rate = 50,
+        arc = 500,
+        time,
+        count,
+        size = 2,
+        speed = 10,
+        lights = new Array,
+        colors = ['#eee'];
 
-canvas.setAttribute('width',w);
-canvas.setAttribute('height',h);
+    canvas.setAttribute('width', w);
+    canvas.setAttribute('height', h);
 
-function init() {
-  time = 0;
-  count = 0;
+    function init() {
+        time = 0;
+        count = 0;
 
-  for(var i = 0; i < arc; i++) {
-    lights[i] = {
-      x: Math.ceil(Math.random() * w),
-      y: Math.ceil(Math.random() * h),
-      toX: Math.random() * 5 + 1,
-      toY: Math.random() * 5 + 1,
-      c: colors[Math.floor(Math.random()*colors.length)],
-      size: Math.random() * size
+        for (var i = 0; i < arc; i++) {
+            lights[i] = {
+                x: Math.ceil(Math.random() * w),
+                y: Math.ceil(Math.random() * h),
+                toX: Math.random() * 5 + 1,
+                toY: Math.random() * 5 + 1,
+                c: colors[Math.floor(Math.random() * colors.length)],
+                size: Math.random() * size
+            }
+        }
     }
-  }
-}
 
-function bubble() {
-  ctx.clearRect(0,0,w,h);
+    function bubble() {
+        ctx.clearRect(0, 0, w, h);
 
-  for(var i = 0; i < arc; i++) {
-    var li = lights[i];
+        for (var i = 0; i < arc; i++) {
+            var li = lights[i];
 
-    ctx.beginPath();
-    ctx.arc(li.x,li.y,li.size,0,Math.PI*2,false);
-    ctx.fillStyle = li.c;
-    ctx.fill();
+            ctx.beginPath();
+            ctx.arc(li.x, li.y, li.size, 0, Math.PI * 2, false);
+            ctx.fillStyle = li.c;
+            ctx.fill();
 
-    li.x = li.x + li.toX * (time * 0.05);
-    li.y = li.y + li.toY * (time * 0.05);
+            li.x = li.x + li.toX * (time * 0.05);
+            li.y = li.y + li.toY * (time * 0.05);
 
-    if(li.x > w) { li.x = 0; }
-    if(li.y > h) { li.y = 0; }
-    if(li.x < 0) { li.x = w; }
-    if(li.y < 0) { li.y = h; }
-  }
-  if(time < speed) {
-    time++;
-  }
-  timerID = setTimeout(bubble,1000/rate);
-}
-init();
-bubble();
+            if (li.x > w) { li.x = 0; }
+            if (li.y > h) { li.y = 0; }
+            if (li.x < 0) { li.x = w; }
+            if (li.y < 0) { li.y = h; }
+        }
+        if (time < speed) {
+            time++;
+        }
+        timerID = setTimeout(bubble, 1000 / rate);
+    }
+    init();
+    bubble();
 }
 
 document.addEventListener("DOMContentLoaded", init1, false);
